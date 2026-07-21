14b3218b3ee8c18a79c77bed9b82cdfa276cf084
zero-one-code <zerotoalphaone@gmail.com>
2026-07-21 17:02:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1028,7 +1028,7 @@ function makeRiskGroupContent(className, titleTop, imageTop, includeCards) {
     baseImage.className = "risk-base-image";
     baseImage.src = "./assets/people_risk_groups_smooth_highres_transparent.png";
     baseImage.alt = "Risk groups for shingles: older people, and people with weakened immunity, and people with some chronic diseases.";
-    setBox(baseImage, 435, imageTop, 1050, 689);
+    setBox(baseImage, 210, imageTop, 1500, 750);
     wrapper.appendChild(baseImage);
 
     if (includeCards) {
